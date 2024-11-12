219e638150dcd5c77425afadc2448f72815f786d
matheus1994 <31566239+matheus1994@users.noreply.github.com>
2024-11-12 10:12:32 -0300

    User.ts
    Signed-off-by: matheus1994 <31566239+matheus1994@users.noreply.github.com>

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,3 +1,19 @@
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
 //TODO Crie a entidade de User
+
+class User{
+
+  private id: number;
+  private firstName: string;
+  private latsName: string;
+  private email: string;
+
+  constructor(id: number, firstName: string, lastName: string, email: string){
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+  }
+  
+}
