9c073dd642343c3908574bcfb49db4ed80af59b6
matheus1994 <31566239+matheus1994@users.noreply.github.com>
2024-11-12 10:01:19 -0300

    Post.ts
    Signed-off-by: matheus1994 <31566239+matheus1994@users.noreply.github.com>

--- src/entity/Post.ts
+++ src/entity/Post.ts
@@ -1,3 +1,17 @@
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
 //TODO Crie a entidade de Post
+
+class Post{
+
+  private id: number;
+  private title: string;
+  private description: string;
+
+  constructor(id: number, title: string, description: string){
+      this.id = id;
+      this.string = string;
+      this.description = description;
+  }
+  
+}
