e0b92e762b102a9ed7b74a0320e6075f1ebfc6c6
matheus1994 <31566239+matheus1994@users.noreply.github.com>
2024-11-12 09:50:53 -0300

    init.sql
    Signed-off-by: matheus1994 <31566239+matheus1994@users.noreply.github.com>

--- init.sql
+++ init.sql
@@ -1,5 +1,22 @@
 USE test_db;
 
 --TODO Crie a tabela de user;
+  create table user(
+    id int AUTO_INCREMENT,
+    firstName varchar(100) NOT NULL,
+    lastName varchar(100) NOT NULL,
+    email varchar(100) NOT NULL,
+    PRIMARY KEY(id)
+  );
 
 --TODO Crie a tabela de posts;
+  create table posts(
+    id int AUTO_INCREMENT,
+    title varchar(100) NOT NULL,
+    description varchar(100) NOT NULL,
+    userId int NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(userId) REFERENCES user(id)
+  );
+    
+    
